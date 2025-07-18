86a347643aa89a0102e4a959bc126587112d1eeb
2274880117@qq.com <>
2025-07-18 17:30:25 -0400

    robotiq_2f_85 update

---
+++ ur5.py
@@ -0,0 +1,16 @@
+class UR5:
+    def __init__(self, ip):
+        self.ip = ip
+        self.rtde_r, self.rtde_c = None, None
+    
+    def moveJ(self, pos, speed, acceleration):
+        self.rtde_c.moveJ(pos, speed, acceleration)
+
+    def servoJ(self, pos, speed, acceleration, look_ahead, gain):
+        self.rtde_c.servoJ(pos, speed, acceleration, self.dt, look_ahead, gain)
+
+    def get_joint_state(self):
+        return self.rtde_r.getActualQ()
+
+    def get_ee_state(self):
+        return self.rtde_r.getActualTCPPose()
--- ur5_robotiq_2f_85_control.py
+++ ur5_robotiq_2f_85_control.py
@@ -15,15 +15,15 @@ from robotiq_gripper import RobotiqGripper
 from robotiq_gripper_control import RobotiqGripper as RobotiqGripperControl
 from scipy.spatial.transform import Rotation as R
 from utils import Colors
+from ur5 import UR5
 
 
-class UR_Robotiq_2f_85_Controller:
-    def __init__(self, robot_ip="172.17.139.100", dt=1.0 / 500, robot_position=None, init_gripper=False, init_pb=False):
+class UR_Robotiq_2f_85_Controller(UR5):
+    def __init__(self, robot_ip="172.17.139.100", dt=1.0 / 500, init_gripper=False):
         self.ip = robot_ip
         self.dt = dt
         self.rtde_c, self.rtde_r = self.init_robot()
         self.gripper = self.init_gripper() if init_gripper else None
-        self.robot_position = robot_position
         self.traj = None
         self._pb = None
         self.pb_robot_arm = None
@@ -38,72 +38,6 @@ class UR_Robotiq_2f_85_Controller:
         self.actual_ee_rot = []
         self.desired_ee_rot = []
         self.tool_z_offset = 0.21
-        if init_pb:
-            self.init_pb_env()
-
-    def init_pb_env(self):
-        self._pb = bullet_client.BulletClient(connection_mode=pb.DIRECT)
-        self._pb.setAdditionalSearchPath(pybullet_data.getDataPath())
-        self._pb.setTimeStep(self.dt)
-        print(Colors.GREEN, "Pybullet initialized", Colors.RESET)
-        self.reset_pb_env()
-        print(Colors.GREEN, "Pybullet env reset", Colors.RESET)
-
-    def reset_pb_env(self):
-        ''' reset Pybullet env '''
-        self._pb.resetSimulation()
-        self._pb.setGravity(0, 0, -9.8)
-        self.pb_base_offset = self.robot_position
-        # Load two UR5e
-        self.pb_robot_arm = self._pb.loadURDF(
-            os.path.join(os.path.join(root_path, "motion_planning"), "assets/ur5e/ur5e.urdf"),
-            basePosition=self.pb_base_offset, useFixedBase=True)
-        self.pb_robot_joints = []
-        for i in range(self._pb.getNumJoints(self.pb_robot_arm)):
-            info = self._pb.getJointInfo(self.pb_robot_arm, i)
-            joint_id = info[0]
-            joint_name = info[1].decode("utf-8")
-            joint_type = info[2]
-            # if joint_name == "ee_fixed_joint":
-            if joint_name == "flange-tool0":
-                self.pb_robot_ee_id = joint_id
-            if joint_type == pb.JOINT_REVOLUTE:
-                self.pb_robot_joints.append(joint_id)
-        self._pb.enableJointForceTorqueSensor(self.pb_robot_arm, self.pb_robot_ee_id, 1)
-
-    def pb_get_pos(self, joint_state):
-        ''' joint state -> pose state '''
-        for joint_id in range(len(self.pb_robot_joints)):
-            self._pb.setJointMotorControl2(
-                bodyUniqueId=self.pb_robot_arm,
-                jointIndex=self.pb_robot_joints[joint_id],
-                controlMode=pb.POSITION_CONTROL,
-                targetPosition=joint_state[joint_id],
-                positionGain=1,
-                maxVelocity=3.14,
-            )
-        step = 0
-        max_step = 500
-        while True:
-            error = np.max(np.abs(
-                np.array([self._pb.getJointState(self.pb_robot_arm, i)[0] for i in self.pb_robot_joints]) - np.array(
-                    joint_state)))
-            self._pb.stepSimulation()
-            step += 1
-            if step > max_step or error < EPSILON:
-                break
-        ee_pos, ee_rot = pb.getLinkState(
-            self.pb_robot_arm, self.pb_robot_ee_id
-        )[:2]
-        return ee_pos, ee_rot
-
-    def init_robot(self):
-        rtde_c = rtde_control.RTDEControlInterface(self.ip)
-        rtde_r = rtde_receive.RTDEReceiveInterface(self.ip)
-        actual_q = rtde_r.getActualQ()
-        print("Init q: ", actual_q)
-        actual_q = rtde_r.getActualQ()
-        return rtde_c, rtde_r
 
     def init_gripper(self):
         gripper = RobotiqGripper(self.ip, 63352)
@@ -156,87 +90,21 @@ class UR_Robotiq_2f_85_Controller:
         else:
             return False
 
-    def control_with_servoJ(self, steps, look_ahead=0.03, gain=200, speed=0.5, acceleration=0.5,
-                            fixed_dt: bool = True, test_gripper_communication=False):  # gain=500
-        self.actual_positions = []
-        self.desired_positions = []
-        self.actual_ee_positions = []
-        self.desired_ee_positions = []
-        self.actual_ee_rot = []
-        self.desired_ee_rot = []
+    # def moveJ(self, pos, speed, acceleration):
+    #     self.rtde_c.moveJ(pos, speed, acceleration)
 
-        start_time = time.time()
-        for i in range(steps):
-            waypoint = self.traj[i]
-            pos = waypoint.position[:6].cpu().tolist()
-            self.desired_positions.append(pos)
-            pos[0] += np.pi / 2
-            self.rtde_c.servoJ(pos, speed, acceleration, self.dt, look_ahead, gain)
-            time.sleep(self.dt)
-            actual_q = self.rtde_r.getActualQ()
-            actual_ee = self.rtde_r.getActualTCPPose()
-            actual_ee[2] += self.tool_z_offset
-            self.actual_positions.append(actual_q)
-            rot = R.from_rotvec(actual_ee[3:])
-            actual_rot = rot.as_euler("ZYX", degrees=True)
-            self.actual_ee_positions.append(actual_ee[:3])
-            self.actual_ee_rot.append(actual_rot.tolist())
-        duration = time.time() - start_time
+    # def servoJ(self, pos, speed, acceleration, look_ahead, gain):
+    #     self.rtde_c.servoJ(pos, speed, acceleration, self.dt, look_ahead, gain)
 
-        time.sleep(0.5)
-        for i in range(steps - 1, -1, -1):
-            waypoint = self.traj[i]
-            pos = waypoint.position[:6].cpu().tolist()
-            self.desired_positions.append(pos)
-            pos[0] += np.pi / 2
-            if test_gripper_communication:
-                self.close_gripper()
-            self.rtde_c.servoJ(pos, speed, acceleration, self.dt, look_ahead, gain)
-            time.sleep(self.dt)
-            if test_gripper_communication:
-                self.open_gripper()
-            actual_q = self.rtde_r.getActualQ()
-            actual_ee = self.rtde_r.getActualTCPPose()
-            actual_ee[2] += self.tool_z_offset
-            self.actual_positions.append(actual_q)
-            rot = R.from_rotvec(actual_ee[3:])
-            actual_rot = rot.as_euler("ZYX", degrees=True)
-            self.actual_ee_positions.append(actual_ee[:3])
-            self.actual_ee_rot.append(actual_rot.tolist())
-        self.open_gripper()
+    # def get_joint_state(self):
+    #     return self.rtde_r.getActualQ()
 
-        if self.init_pb_env:
-            if self._pb is None:
-                self.init_pb()
-            for i in range(len(self.desired_positions)):
-                joint_state = self.desired_positions[i]
-                ee_pos, ee_rot = self.pb_get_pos(joint_state)
-                ee_pos -= self.pb_base_offset
-                rot = R.from_quat(ee_rot)
-                R1 = rot.as_matrix()
-                # R2 = R.from_euler('Z', -np.pi/2)
-                R2 = R.from_euler('Z', -np.pi)
-                rot = R.from_matrix(R2.as_matrix() @ R1)
-                self.desired_ee_positions.append(R2.apply(ee_pos))
-                # self.desired_ee_positions.append(ee_pos)
-                self.desired_ee_rot.append(rot.as_euler("ZYX", degrees=True))
-        return duration
-
-    def moveJ(self, pos, speed, acceleration):
-        self.rtde_c.moveJ(pos, speed, acceleration)
-
-    def servoJ(self, pos, speed, acceleration, look_ahead, gain):
-        self.rtde_c.servoJ(pos, speed, acceleration, self.dt, look_ahead, gain)
-
-    def get_joint_state(self):
-        return self.rtde_r.getActualQ()
-
-    def get_ee_state(self):
-        return self.rtde_r.getActualTCPPose()
+    # def get_ee_state(self):
+    #     return self.rtde_r.getActualTCPPose()
 
 def single_arm_test():
     dt = 0.002
-    controller0 = UR_Robotiq_2f_85_Controller(dt=dt, robot_ip="172.17.139.100", init_pb=False, init_gripper=True)
+    controller0 = UR_Robotiq_2f_85_Controller(dt=dt, robot_ip="172.17.139.100", init_gripper=True)
     # controller0.close_gripper()
     # controller0.open_gripper()
 
